7cc406abebef228613a3b7c2d2b0d6261c1e7474
padminiparvathy <75385047+padminiparvathy@users.noreply.github.com>
2021-06-07 13:49:57 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("REVBAILA", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+    textSize(15)
+    text("CUTNIFON", 650,200)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 650,220)
+
+
+fill("white")
+textSize(15)
+text("ATADASBA", 200,350)
+fill("lightblue")
+text("Hint: Store all information !!", 200,370)
+
+
+}
